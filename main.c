3410630095482c71cf9a454145c8dd7a7fe2a6f8
Igor Soares <igsoares89@gmail.com>
2026-05-07 15:33:45 -0300

    Adicionado códigos de cores ANSI e função limpar()

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-#define COLOR_MAGENTA "\033[0;35m"
-#define COLOR_RED   "\033[0;31m"
-#define COLOR_GREEN   "\033[0;32m"
-#define RESET "\033[0m"
+#define COLOR_MAGENTA   "\033[0;35m"
+#define COLOR_RED       "\033[0;31m"
+#define COLOR_GREEN     "\033[0;32m"
+#define COLOR_YELLOW    "\033[0;33m"
+#define COLOR_BLUE      "\033[0;34m"
+#define COLOR_PURPLE    "\033[0;35m"
+#define COLOR_CYAN      "\033[0;36m"
+#define RESET           "\033[0m"
+
+void limpar() {
+    system("clear");
+}
 
 void tela_inicial(){
 
     int op;
+    limpar();
 
-    system("clear");
     printf(COLOR_GREEN "\n\nEscolha a opção: 1 - Iniciar   0 - Sair\n\n" RESET "Escolha a opção: ");
     scanf("%d",&op);
-    system("clear");
+    limpar();
     if(op){
         printf(COLOR_MAGENTA "\n\nBem-vindo ao jogo\n\n" RESET);
     }else{
