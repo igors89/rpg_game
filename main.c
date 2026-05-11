cf0fd5ab895c324fd51ae731dd34627ad310c3ab
IGOR ANDRADE SOARES <igsoares89@gmail.com>
2026-05-11 07:54:36 -0300

    commit automatico

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "unistd.h"
+
+#ifdef _WIN32
+    #include "windows.h"
+#else
+    #include "unistd.h"
+#endif
 
 #define COLOR_MAGENTA   "\033[0;35m"
 #define COLOR_RED       "\033[0;31m"
@@ -9,15 +14,20 @@
 #define COLOR_BLUE      "\033[0;34m"
 #define COLOR_PURPLE    "\033[0;35m"
 #define COLOR_CYAN      "\033[0;36m"
-#define RESET           "\033[0m"
+#define DEFAULT           "\033[0m"
 
 #define LINHAS 7
 #define COLUNAS 50
 
 char tela[LINHAS][COLUNAS];
+int i,j;
 
 void limpar() {
-    system("clear");
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
 
 void press_enter(){
@@ -25,11 +35,10 @@ void press_enter(){
     getchar();
 }
 
-void renderizar_caixa() {
+void montar_caixa() {
     
-    int i,j;
     for(i=0; i<LINHAS; i++){
-        for(j=0, j<COLUNAS, j++){
+        for(j=0; j<COLUNAS; j++){
             if( i==0 || i < LINHAS-1) tela[i][j]='-';
             else if (j == 0 || j == COLUNAS - 1) tela[i][j] = '|';
             else tela[i][j]=' ';
@@ -44,17 +53,24 @@ void renderizar_caixa() {
 
 void tela_inicial(){
 
+    #ifdef _WIN32
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+    #endif
+
     int op;
     limpar();
 
-    renderizar_caixa();
-    printf(COLOR_GREEN "\n\n 1 - Iniciar   0 - Sair\n\n" RESET "Escolha a opção: ");
+    montar_caixa();
+    printf(COLOR_GREEN "\n\n 1 - Iniciar   0 - Sair\n\n" DEFAULT "Escolha a opção: ");
     scanf("%d",&op);
     limpar();
-    if(op){
-        printf(COLOR_MAGENTA "\n\nBem-vindo ao jogo\n\n" RESET);
+    if(op==1){
+        printf(COLOR_MAGENTA "\n\nBem-vindo ao jogo\n\n" DEFAULT);
+    }else if(op==0){
+        printf(COLOR_RED "\n\nSaindo\n\n" DEFAULT);
     }else{
-        printf(COLOR_RED "\n\nSaindo\n\n" RESET);
+        printf(COLOR_RED "\n\nOpção inválida\n\n" DEFAULT);
     }
     getchar();
     press_enter();
@@ -63,12 +79,31 @@ void tela_inicial(){
 int main () {
     limpar();
     printf(COLOR_CYAN);
-    printf("+---------------------------------------+\n");
-    printf("|                                       |\n");
-    printf("|            RPG - THE GAME             |\n");
-    printf("|                                       |\n");
-    printf("+---------------------------------------+\n\n");
-    printf(RESET);
+    char titulo[] = "RPG - THE GAME";
+    int centro = sizeof(titulo)/2;
+    for(i=0; i<5; i++){
+        for(j=0; j<30; j++){
+            if( i==0 || i == 4) tela[i][j]='-';
+            else if (j == 0 || j == 29) tela[i][j] = '|';
+            else tela[i][j]=' ';
+        }
+    }
+    tela[0][0] = '+';
+    tela[0][30-1] = '+';
+    tela[5 - 1][0] = '+';
+    tela[5 - 1][30 - 1] = '+';
+    int linha_texto = 2;
+    int coluna_inicial = 16-centro;
+    for(int k=0; k < sizeof(titulo); k++){
+        tela[linha_texto][coluna_inicial+k] = titulo[k];
+    }
+    for(i=0; i<LINHAS;i++){
+        for(j=0;j<COLUNAS;j++){
+            printf("%c",tela[i][j]);
+        }
+        printf("\n");
+    }
+    printf(DEFAULT);
     press_enter();
     tela_inicial();
     return 0;
